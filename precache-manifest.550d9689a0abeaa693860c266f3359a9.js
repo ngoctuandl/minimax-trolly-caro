2ef22dfaedb9ac83feeab076090a22d1b6c71aed
pptv <pptv@PPTVK-D048.toyo-local.co.jp>
2019-07-10 16:41:45 +0700

    Updates

---
+++ precache-manifest.550d9689a0abeaa693860c266f3359a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4387decd36e224a5eaaf769dd04b4b07",
+    "url": "/minimax-trolly-caro/index.html"
+  },
+  {
+    "revision": "6ec6c15c62ba02386a09",
+    "url": "/minimax-trolly-caro/static/js/2.4298fcdc.chunk.js"
+  },
+  {
+    "revision": "26b0e801226d894475cf",
+    "url": "/minimax-trolly-caro/static/js/main.3bf035c9.chunk.js"
+  },
+  {
+    "revision": "5f8062d72d067ff838c3",
+    "url": "/minimax-trolly-caro/static/js/runtime~main.bac64b59.js"
+  }
+]);
